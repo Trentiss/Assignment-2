b3bac6770c2d2bfafef00e1f094bc2b1fc594e7d
Trent Christy <tchristy99@outlook.com>
2020-02-24 21:16:12 -0500

    First Commit - Contains completed program and class

---
+++ TCAssignment2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit;
+            int option;
+            string[] triDimensions;
+            exit = false;
+            do
+            {
+                Console.WriteLine("Please Select an Option: " +
+                    "\n1. Enter a triangle dimensions " +
+                    "\n2. Exit");
+                try
+                {
+                    option = int.Parse(Console.ReadLine());
+                    if (option > 2 || option < 0)
+                    {
+                        throw new Exception("Input must be greater than 0 and less than 2");
+                    }
+                    switch (option)
+                    {
+                        case 1:
+                            triDimensions = new string[3];
+                            for (int i = 0; i < triDimensions.Length; i++)
+                            {
+                                Console.Write($"Please Enter Number {i + 1}: ");
+                                triDimensions[i] = Console.ReadLine();
+                            }
+                            Console.WriteLine(TriangleSolver.Analyze(triDimensions));
+                            Console.WriteLine();
+                            break;
+                        case 2:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            } while (!exit);
+        }
+    }
+}
---
+++ TCAssignment2/TriangleSolver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCAssignment2
+{
+    public static class TriangleSolver
+    {
+        public static string Analyze(string[] triDimensions)
+        {
+            string returnMessage = "";
+            int[] measurements = new int[triDimensions.Length];
+
+            for (int i = 0; i < triDimensions.Length; i++)
+            {
+                bool parsed = Int32.TryParse(triDimensions[i], out measurements[i]);
+                if (!parsed)
+                {
+                    returnMessage = "Not a triangle : One or more values are not numeric";
+                }
+            }
+            if (returnMessage == "")
+            {
+                if (triDimensions[0] == triDimensions[1] && triDimensions[0] == triDimensions[2])
+                {
+                    returnMessage = "This is a triangle : It is Equilateral";
+                }
+                else if (triDimensions[0] != triDimensions[1] && triDimensions[0] != triDimensions[2] && triDimensions[1] != triDimensions[2])
+                {
+                    returnMessage = "This is a triangle : It is Scalene";
+                }
+                else if (triDimensions[0] == triDimensions[1] || triDimensions[0] == triDimensions[2] || triDimensions[1] == triDimensions[2])
+                {
+                    returnMessage = "This is a triangle : It is Isoceles";
+                }
+            }
+            return returnMessage;
+        }
+    }
+}
